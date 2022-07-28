41db06779ef35efd745a8a4356a3eb80186e677c
Abhishek Yadav <abhishekyadav2k16@gmail.com>
2022-07-28 22:05:18 +0530

    Sorting function

---
+++ SortingFunction.java
@@ -0,0 +1,18 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class SortingFunction {
+
+	public static void main(String[] args) {
+	
+		int arr[]= {4,6,7,8,12,45,67,0};
+		char arr1[] = {'k','c','d','e'};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr1);
+		System.err.println(Arrays.toString(arr1));
+
+	}
+
+}
